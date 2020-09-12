6122108f47d95e46be6e741d2a2d7773d3983c1f
pericles-jr <pericles-terto@hotmail.com>
2020-09-12 09:56:44 -0300

    coordinator protocol

---
+++ ItiInvest/Shared/AppCoordinator.swift
@@ -0,0 +1,13 @@
+//
+//  AppCoordinator.swift
+//  ItiInvest
+//
+//  Created by Pericles Junor on 12/09/20.
+//  Copyright © 2020 DevBoostItau. All rights reserved.
+//
+
+import UIKit
+
+class AppCoordinator: NSObject {
+
+}
---
+++ ItiInvest/Shared/BaseCoordinator.swift
@@ -0,0 +1,9 @@
+//
+//  BaseCoordinator.swift
+//  ItiInvest
+//
+//  Created by Pericles Junor on 12/09/20.
+//  Copyright © 2020 DevBoostItau. All rights reserved.
+//
+
+import Foundation
