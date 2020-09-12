9459eebb8844d5abaf625d0833855269db13119b
ghepaul <gabrielhenriquepaul@correio.itau.com.br>
2020-09-12 13:37:41 -0300

    altera cores

--- ItiInvest/Constants/ITIColor.swift
+++ ItiInvest/Constants/ITIColor.swift
@@ -13,7 +13,8 @@ struct ITIColor {
     static let purple = UIColor(red: 255/255, green: 0, blue: 137/255, alpha: 1)
     static let pink = UIColor(red: 255/255, green: 0, blue: 126/255, alpha: 1)
     static let black = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-    static let gray = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+    static let gray = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+    static let lightGray = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
     static let green = #colorLiteral(red: 0.3376246095, green: 0.6866923571, blue: 0.00485552242, alpha: 1)
     static let red = #colorLiteral(red: 0.998283565, green: 0.003100437112, blue: 0.001940896618, alpha: 1)
     
