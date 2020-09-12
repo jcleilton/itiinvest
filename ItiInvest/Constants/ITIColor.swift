6637f52c0d6a5b91e3676477b6e083b58250be86
ghepaul <gabrielhenriquepaul@correio.itau.com.br>
2020-09-12 11:23:25 -0300

    adiciona cores da tela de detalhes

--- ItiInvest/Constants/ITIColor.swift
+++ ItiInvest/Constants/ITIColor.swift
@@ -12,4 +12,13 @@ struct ITIColor {
     static let orange = UIColor(red: 245/255, green: 101/255, blue: 0, alpha: 1)
     static let purple = UIColor(red: 255/255, green: 0, blue: 137/255, alpha: 1)
     static let pink = UIColor(red: 255/255, green: 0, blue: 126/255, alpha: 1)
+    static let black = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    static let gray = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+    static let green = #colorLiteral(red: 0.3376246095, green: 0.6866923571, blue: 0.00485552242, alpha: 1)
+    static let red = #colorLiteral(red: 0.998283565, green: 0.003100437112, blue: 0.001940896618, alpha: 1)
+    
+    static let mainText = black
+    static let secondaryText = gray
+    static let greenText = green
+    static let redText = red
 }
