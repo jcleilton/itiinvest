ce90245a5cf9d37c01f1f1088da8f1139a864696
jcleilton <josefeitosa@brq.com>
2020-09-19 18:42:33 -0300

    Real time firebase

---
+++ ItiInvest/Models/FIRStock.swift
@@ -0,0 +1,15 @@
+//
+//  FIRStock.swift
+//  ItiInvest
+//
+//  Created by Cleilton Feitosa on 19/09/20.
+//  Copyright © 2020 DevBoostItau. All rights reserved.
+//
+
+import Foundation
+
+struct FIRStock {
+    let id: String
+    let name: String
+    let symbol: String
+}
--- ItiInvest/Service/Service.swift
+++ ItiInvest/Service/Service.swift
@@ -35,7 +35,6 @@ class Service{
             }
         })
         task.resume()
-
     }
     
     
