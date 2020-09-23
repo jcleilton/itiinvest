49820334753ac9a0f1d182ea0d580dbc36232d7c
jcleilton <josefeitosa@brq.com>
2020-09-23 20:30:44 -0300

    Testes

---
+++ ItiInvestUISnapshotTests/Home/HomeUISnapshotTests.swift
@@ -0,0 +1,24 @@
+//
+//  HomeUISnapshotTests.swift
+//  ItiInvestUISnapshotTests
+//
+//  Created by Cleilton Feitosa on 23/09/20.
+//  Copyright © 2020 DevBoostItau. All rights reserved.
+//
+
+import XCTest
+
+class HomeUISnapshotTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testUISnapshot() throws {
+        
+    }
+}
