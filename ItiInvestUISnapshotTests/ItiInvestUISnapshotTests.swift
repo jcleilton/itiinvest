3fb88f08ca6f32d173c990b3b032feeb1eeff3b9
jcleilton <josefeitosa@brq.com>
2020-09-23 20:20:37 -0300

    Comecei classes de testes de ui snapshot

---
+++ ItiInvestUISnapshotTests/ItiInvestUISnapshotTests.swift
@@ -0,0 +1,33 @@
+//
+//  ItiInvestUISnapshotTests.swift
+//  ItiInvestUISnapshotTests
+//
+//  Created by Cleilton Feitosa on 23/09/20.
+//  Copyright © 2020 DevBoostItau. All rights reserved.
+//
+
+import XCTest
+
+class ItiInvestUISnapshotTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
